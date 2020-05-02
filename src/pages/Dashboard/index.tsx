3b8a2d1aa40f77fd60b98c052595d588df77d1e1
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-05-02 18:31:05 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logo from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logo} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+      <Form>
+        <input placeholder="Digite aqui o repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/31170322?s=400&u=72e8e25a7e2a38c4f7f0327c4edd335733f959ac&v=4"
+            alt="lihdi"
+          />
+          <div>
+            <strong>Opencode</strong>
+            <p>Blog para compartilhar conhecimento</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
