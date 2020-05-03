b09504394df7035ad5e0219b4afdf1a49a634a2d
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-05-03 09:20:12 -0300

    Navegando entre rotas

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
@@ -68,9 +69,9 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
       <Repositories>
         {repositories.map((repository) => (
-          <a
+          <Link
             key={repository.full_name}
-            href={`https://github.com/${repository.full_name}`}
+            to={`repositories/${repository.full_name}`}
           >
             <img
               src={repository.owner.avatar_url}
@@ -81,7 +82,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
