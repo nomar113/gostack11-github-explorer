9417b0490185d9cdf5bf996949b98d509d1d45a7
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-05-03 10:23:40 -0300

    Estilizando página de detalhes do repositório

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +11,53 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/34141267?s=460&u=3d13ddaaf7d1aff4711edde9c860a80fc67c284f&v=4"
+            alt=""
+          />
+          <div>
+            <strong>full_name</strong>
+            <p>description</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>333</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>4999</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>999</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to={`repositories`}>
+          <div>
+            <strong>epository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
